87a29be777cf01e0c15592344d00b28745e85c2c
bocai <1474286853@qq.com>
2017-11-22 11:53:59 +0800

    Initial Commit

---
+++ runtime知识点/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtime知识点
+//
+//  Created by songqingbo on 2017/11/22.
+//  Copyright © 2017年 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
